c66c65f02475f92adbcb52ed69a318a34fa3513d
Zeus6768 <akaps6768@gmail.com>
2023-10-17 08:30:36 +0900

    문제7: 모든 이름을 순회하며 점수를 계산한다. 가장 점수가 높은 다섯 명을 반환한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,13 +4,23 @@ import java.util.*;
 
 public class Problem7 {
 
-    private static List<String> answer = Collections.emptyList();
+    private static final int SHARED_FRIEND_SCORE = 10;
+
+    private static List<String> answer = new ArrayList<>();
     private static Map<String, Set<String>> relationships = new HashMap<>();
-    private static Map<String, Integer> scores = new TreeMap<>();
+    private static Map<String, Integer> scores = new HashMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         initializeRelationships(friends);
         initializeScores(user, visitors);
+        applySharedFriendScore(user);
+        List<String> scoreKeys = sortedKeysOf();
+        for (int i = 0; i < 5 && i < scoreKeys.size(); i++) {
+            String name = scoreKeys.get(i);
+            if (scores.get(name) != 0){
+                answer.add(name);
+            }
+        }
         return answer;
     }
 
@@ -27,12 +37,39 @@ public class Problem7 {
 
     private static void initializeScores(String user, List<String> visitors) {
         for (String visitor : visitors) {
-            scores.putIfAbsent(visitor, 0);
+            if (!relationships.get(user).contains(visitor)) {
+                scores.computeIfPresent(visitor, (k, v) -> v + 1);
+                scores.putIfAbsent(visitor, 1);
+            }
         }
         for (String name : relationships.keySet()) {
             if (!(name.equals(user) || relationships.get(user).contains(name))) {
                 scores.putIfAbsent(name, 0);
             }
         }
+    }
+
+    private static void applySharedFriendScore(String user) {
+        for (String candidate : scores.keySet()) {
+            if (relationships.containsKey(candidate)) {
+                scores.compute(candidate, (k, v) -> v + sharedFriendCountOf(user, candidate) * SHARED_FRIEND_SCORE);
+            }
+        }
+    }
+
+    private static int sharedFriendCountOf(String user, String candidate) {
+        Set<String> sharedFriends = new HashSet<>(relationships.get(user));
+        sharedFriends.retainAll(relationships.get(candidate));
+        return sharedFriends.size();
+    }
+
+    private static List<String> sortedKeysOf() {
+        List<String> scoreKeys = new ArrayList<>(scores.keySet());
+        scoreKeys.sort((o1, o2) -> {
+            if (scores.get(o1).compareTo(scores.get(o2)) == 0)
+                return o1.compareTo(o2);
+            return scores.get(o2).compareTo(scores.get(o1));
+        });
+        return scoreKeys;
     }
 }

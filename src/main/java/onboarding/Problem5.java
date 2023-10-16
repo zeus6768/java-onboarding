1e437a3a3c2cf6f4edb149c4564ac61ac8686aed
Zeus6768 <akaps6768@gmail.com>
2023-10-16 13:56:42 +0900

    문제5: 큰 단위부터 작은 단위 순서로 나눈 후 그 몫을 결과 배열에 저장하고 반환한다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+        List<Integer> units = List.of(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1);
+        for (int i = 0; i < units.size(); i++) {
+            answer.add(money / units.get(i));
+            money %= units.get(i);
+        }
         return answer;
     }
 }

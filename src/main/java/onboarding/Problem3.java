8dc97c9df95f14fdb504ada88e9443ae24fc6631
Zeus6768 <akaps6768@gmail.com>
2023-10-16 13:25:03 +0900

    문제3: 1부터 number까지 증가하며, 각 자릿수에 3, 6 또는 9가 포함된 개수를 세어 반환한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
+        final List<Integer> number369 = List.of(3, 6, 9);
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            int tmp = i;
+            while (tmp != 0) {
+                if (number369.contains(tmp % 10)) {
+                    answer++;
+                }
+                tmp /= 10;
+            }
+        }
         return answer;
     }
 }

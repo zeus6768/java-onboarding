08b05eb28405a88f54c88a5f660f7b69e7b308f9
Zeus6768 <akaps6768@gmail.com>
2023-10-16 15:34:57 +0900

    문제7: 친구 후보들의 점수를 0으로 초기화한다. 이때 친구 후보가 될 수 있는지 검증한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,9 +6,11 @@ public class Problem7 {
 
     private static List<String> answer = Collections.emptyList();
     private static Map<String, Set<String>> relationships = new HashMap<>();
+    private static Map<String, Integer> scores = new TreeMap<>();
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         initializeRelationships(friends);
+        initializeScores(user, visitors);
         return answer;
     }
 
@@ -20,6 +22,17 @@ public class Problem7 {
             relationships.get(a).add(b);
             relationships.putIfAbsent(b, new HashSet<>());
             relationships.get(b).add(a);
+        }
+    }
+
+    private static void initializeScores(String user, List<String> visitors) {
+        for (String visitor : visitors) {
+            scores.putIfAbsent(visitor, 0);
+        }
+        for (String name : relationships.keySet()) {
+            if (!(name.equals(user) || relationships.get(user).contains(name))) {
+                scores.putIfAbsent(name, 0);
+            }
         }
     }
 }

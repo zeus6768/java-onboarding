ca71388b032a90f9a774892f4c95ddc00b8dbf1e
Zeus6768 <akaps6768@gmail.com>
2023-10-16 14:29:49 +0900

    문제6: 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자들의 이메일 목록을 반환한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,24 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> answer = new TreeSet<>();
+        Map<String, String> nicknames = new HashMap<>();
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String substring = nickname.substring(i, i+2);
+                if (nicknames.containsKey(substring)) {
+                    answer.add(email);
+                    answer.add(nicknames.get(substring));
+                } else {
+                    nicknames.put(substring, email);
+                }
+            }
+        }
+        return new ArrayList<>(answer);
     }
 }

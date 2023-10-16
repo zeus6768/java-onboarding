535f9256f88fc52f76e3b72dec663cb531a34025
Zeus6768 <akaps6768@gmail.com>
2023-10-16 13:39:34 +0900

    문제4: 주어진 사전에 따라 알파벳을 변환하여 반환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            answer.append(translate(c));
+        }
+        return answer.toString();
+    }
+
+    private static char translate(char c) {
+        if (65 <= (int) c && (int) c <= 90) {
+            return (char) (155 - (int) c);
+        }
+        if (97 <= (int) c && (int) c <= 122) {
+            return (char) (219 - (int) c);
+        }
+        return c;
     }
 }

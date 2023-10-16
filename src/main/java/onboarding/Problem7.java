1da215cfe6ca2cc618a2d23936bd4e00a5e10bbd
Zeus6768 <akaps6768@gmail.com>
2023-10-16 15:34:14 +0900

    문제7: 친구 관계도를 초기화한다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,25 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static List<String> answer = Collections.emptyList();
+    private static Map<String, Set<String>> relationships = new HashMap<>();
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        initializeRelationships(friends);
         return answer;
+    }
+
+    private static void initializeRelationships(List<List<String>> friends) {
+        for (List<String> friend : friends) {
+            String a = friend.get(0);
+            String b = friend.get(1);
+            relationships.putIfAbsent(a, new HashSet<>());
+            relationships.get(a).add(b);
+            relationships.putIfAbsent(b, new HashSet<>());
+            relationships.get(b).add(a);
+        }
     }
 }

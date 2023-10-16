20c40390ee09d64930222c2d3df9f0e20d65ebf4
Zeus6768 <akaps6768@gmail.com>
2023-10-16 10:27:06 +0900

    문제2: 문자열에 중복이 있는지 검증한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,17 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    private static boolean isRedundant(String string) {
+        if (!string.isEmpty()) {
+            char previous = string.charAt(0);
+            for (int i = 1; i < string.length(); i++) {
+                if (previous == string.charAt(i)) {
+                    return true;
+                }
+                previous = string.charAt(i);
+            }
+        }
+        return false;
+    }
 }

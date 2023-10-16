09b11a766fedc2eebf1c0ee3bb47f5f7383e8769
Zeus6768 <akaps6768@gmail.com>
2023-10-16 10:21:05 +0900

    미션1: 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수인지 검증한다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -27,6 +27,10 @@ class Problem1 {
         return 0 < pages.get(0) && pages.get(0) <= 400 && 0 < pages.get(1) && pages.get(1) <= 400;
     }
 
+    private static boolean isLeftPageOdd(List<Integer> pages) {
+        return pages.get(0) % 2 == 1;
+    }
+
     private static int scoreOf(List<Integer> player) {
         int leftPage = Math.max(addPageDigits(player.get(0)), multiplyPageDigits(player.get(0)));
         int rightPage = Math.max(addPageDigits(player.get(1)), multiplyPageDigits(player.get(1)));
